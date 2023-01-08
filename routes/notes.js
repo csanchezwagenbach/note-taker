ef5a613bf9050fad4fe823c51f747dc8b52dbd4c
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-01-08 14:36:28 -0800

    wrote route for api/notes post requests, post route working as expected

--- routes/notes.js
+++ routes/notes.js
@@ -9,4 +9,22 @@ notes.get("/", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
 });
 
+notes.post("/", (req, res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        newNote = {
+            title,
+            text
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        res.json(`New note successfully added`);
+    } else {
+        res.errored(`Error in adding new note`)
+    }
+});
+
 module.exports = notes;
